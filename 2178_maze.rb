ea3da6cafb3eb5cafbd0129d8676961030e61165
Elixir Kook <justkook@gmail.com>
2019-04-03 00:23:15 +0900

    2178_maze.rb: added escape condition for the shortest way ,and shorten code.

--- 2178_maze.rb
+++ 2178_maze.rb
@@ -2,21 +2,17 @@
 # https://www.acmicpc.net/problem/2178
 
 $n,$m = gets.split.map {|c| c.to_i}
-
 $maze = Array.new $n
-$d_map = Array.new($n) { Array.new($m) {0} }
-
+#$d_map = Array.new($n) { Array.new($m) {0} }
+$d_map = Array.new($n) { Array.new($m) }
 (0..($n-1)).each do |i|
     $maze[i] = gets.chomp.split ''
 end
 
 def go_further(i, j, c)
-    return if i<0 or j<0
-    return if i>=$n or j>=$m
+    return if i<0 or j<0 or i>=$n or j>=$m
     return unless $maze[i][j]=='1'
-    return unless $d_map[i][j]==0
-    #puts "i=#{i}, j=#{j}, maze[i][j]=#{$maze[i][j]}, $d_map[i][j]=#{$d_map[i][j]}"
-
+    return if $d_map[i][j] and $d_map[i][j]<c
     c += 1
     $d_map[i][j] = c
     go_further(i-1, j, c)
